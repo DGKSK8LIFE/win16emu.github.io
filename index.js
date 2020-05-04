c3856163b9dc0605e6d68670348ca620d9200590
russon@awty.org <russon@awty.org>
2020-05-04 14:22:47 -0500

    Added a .html file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send("Hello world")
+  res.sendFile("public/index.html", {
+    root: __dirname
+  });
 });
 
 app.listen(9000, () => {
